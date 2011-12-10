7e76626a92b3d1f0d22f12aa4d26af3811004ca7
Giles Burgess <gilesjb@gmail.com>
2011-12-10 01:20:19 -0800

    Fixed indentation

--- static_server.js
+++ static_server.js
@@ -7,41 +7,43 @@ var http = require("http"),
 
 http.createServer(function(request, response) {
 
-  var uri = url.parse(request.url).pathname
-    , filename = path.join(process.cwd(), uri);
-  
-  path.exists(filename, function(exists) {
-    if (!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
-      response.end();
-      return;
-    }
+    var parsed = url.parse(request.url),
+        filename = path.join(process.cwd(), parsed.pathname);
 
-    var rs = fs.createReadStream(filename, {bufferSize: 10});
-    
-    rs.on('error', function(err) {
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
-    });
-    
-    rs.on('open', function() {
-      response.writeHead(200);
-    });
-    
-    rs.on('data', function(data) {
-        response.write(data);
-        rs.pause();
-        timers.setTimeout(function() {
-            rs.resume();
-        }, 10);
-    });
+    console.log(parsed);
 
-    rs.on('end', function() {
-        response.end();
+    path.exists(filename, function(exists) {
+        if (!exists) {
+            response.writeHead(404, {"Content-Type": "text/plain"});
+            response.write("404 Not Found\n");
+            response.end();
+            return;
+        }
+
+        var rs = fs.createReadStream(filename, {bufferSize: 10});
+
+        rs.on('error', function(err) {
+            response.writeHead(500, {"Content-Type": "text/plain"});
+            response.write(err + "\n");
+            response.end();
+        });
+
+        rs.on('open', function() {
+            response.writeHead(200);
+        });
+
+        rs.on('data', function(data) {
+            response.write(data);
+            rs.pause();
+            timers.setTimeout(function() {
+                rs.resume();
+            }, 10);
+        });
+
+        rs.on('end', function() {
+            response.end();
+        });
     });
-  });
 }).listen(parseInt(port, 10));
 
 console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
